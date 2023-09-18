d2e6bb7f236f890cd9761ccfe99fe8fa7599f8a0
Jojokristiadi <100785243+Jojokristiadi@users.noreply.github.com>
2023-09-18 21:31:11 +0700

    learn delete, checked

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,35 +1,42 @@
-import { Checkbox, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
-import React from 'react'
+import {
+  Checkbox,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import React from "react";
 
-const ToDoList = ({ toDoList }) => {
- 
-
+const ToDoList = ({ toDoList, handleCheck }) => {
   return (
-    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-      {
-        toDoList.map((todo, index) => {
-          return <ListItem
-            key={todo}
-
-            disablePadding
-          >
+    <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+      {toDoList.map((todo, index) => {
+        return (
+          <ListItem key={index} disablePadding>
             <ListItemButton role={undefined} dense>
               <ListItemIcon>
                 <Checkbox
                   edge="start"
                   tabIndex={-1}
                   disableRipple
-                  inputProps={{ 'aria-labelledby': index }}
+                  inputProps={{ "aria-labelledby": index }}
+                  checked={todo.completed}
+                  onChange={() => handleCheck(index)}
                 />
               </ListItemIcon>
-              <ListItemText id={index} primary={todo} />
+              <ListItemText id={index} primary={todo.task} className={todo.completed && "strike"}/>
             </ListItemButton>
           </ListItem>
-        })
-      }
-
+        );
+      })}
     </List>
-  )
-}
+  );
+};
 
-export default ToDoList
+export default ToDoList;
+// const orang = {"name": "budi"}
+// orang.name
+// if(umur > 18 && wni) pemilu
+// dua2nya true
+// umur < 18
--- src/containers/ToDo.jsx
+++ src/containers/ToDo.jsx
@@ -1,10 +1,14 @@
+import { Button } from "@mui/material";
 import React, { useState } from "react";
 import ToDoForm from "../components/ToDoForm";
 import ToDoList from "../components/ToDoList";
 
 const ToDo = () => {
+  // state adalah untuk menyimpan data di halaman tanpa reload.
   const [toDo, setToDo] = useState("");
   const [toDoList, setToDoList] = useState([]);
+  // sebelum: ["sapu", "pel"]
+  // sesudah: [{"task": "sapu", "completed":true}, {"task": "mencuci", "completed":false}]
 
   const handleClick = () => {
     //spread operator ...
@@ -12,16 +16,35 @@ const ToDo = () => {
     if (toDo === null || toDo === "") {
       alert("To do is required!");
     } else {
-      setToDoList([toDo, ...toDoList]);
+      setToDoList([{ task: toDo, completed: false }, ...toDoList]);
       setToDo("");
     }
   };
 
+  const handleCheck = (index) => {
+    const newToDo = [...toDoList];
+    newToDo[index].completed = !newToDo[index].completed;
+    //Untuk set state ulang, kita harus memberikan array utuh
+    //array utuh untuk menimpa setstate secara keseluruhan
+    setToDoList(newToDo);
+  };
+
+  const deleteClick = () => {
+   const filteringObject =  toDoList.filter((el) => {
+     return el.completed === false
+   })
+    setToDoList(filteringObject)
+  }
+
   return (
     <div className="todo">
       <h2 className="title">TO-DO-LIST</h2>
+      {/* fungsi props untuk memeberikan data yang dibutuhkan komponen. */}
       <ToDoForm handleClick={handleClick} setToDo={setToDo} toDo={toDo} />
-      <ToDoList toDoList={toDoList} />
+      <ToDoList toDoList={toDoList} handleCheck={handleCheck} />
+      <Button variant="outlined" color="error" onClick={deleteClick}>
+        Delete
+      </Button>
     </div>
   );
 };
